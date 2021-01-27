708cd19b3e1b0566e49d4e548cf67432bef0c47f
Charly_Angel <c.a.a.a.503@gmail.com>
2021-01-26 23:48:36 -0600

    feat(badge-details): implementación del componente badge-details

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import BadgeDetails from "./pages/BadgeDetails";
 import BadgeNew from "./pages/BadgeNew";
 import BadgeEdit from "./pages/BadgeEdit";
 import Badges from "./pages/Badges";
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
           <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
           <Route component={NotFound} />
         </Switch>
