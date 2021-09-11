44d3b68d94b8c54cb36b97ed4f4027b92d785a1a
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-10 23:36:24 -0500

    refactor: actualizar Badge de class component a function component

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,34 +1,32 @@
-import React from "react";
-import Gravatar from "./Gravatar";
-import "./styles/Badge.css";
+import React from 'react';
+import Gravatar from './Gravatar';
+import './styles/Badge.css';
 
-class Badge extends React.Component {
-  render() {
-    return (
-      <div className="Badge">
-        <div className="Badge__header">
-          <img
-            src="https://static.platzi.com/media/tmp/class-files/git/platzi-badges/platzi-badges-4.Estilos/src/images/badge-header.svg"
-            alt="Logo de la conferencia"
-          />
-        </div>
-
-        <div className="Badge__section-name">
-          <Gravatar estilo="Badge__avatar" email={this.props.email} />
-          <h1>
-            {this.props.firstName} <br /> {this.props.lastName}
-          </h1>
-        </div>
-
-        <div className="Badge__section-info">
-          <h3>{this.props.jobTitle}</h3>
-          <div>@{this.props.twitter}</div>
-        </div>
-
-        <div className="Badge__footer">#platziconf</div>
+function Badge({ firstName, lastName, jobTitle, twitter, email }) {
+  return (
+    <div className='Badge'>
+      <div className='Badge__header'>
+        <img
+          src='https://static.platzi.com/media/tmp/class-files/git/platzi-badges/platzi-badges-4.Estilos/src/images/badge-header.svg'
+          alt='Logo de la conferencia'
+        />
       </div>
-    );
-  }
+
+      <div className='Badge__section-name'>
+        <Gravatar estilo='Badge__avatar' email={email} />
+        <h1>
+          {firstName} <br /> {lastName}
+        </h1>
+      </div>
+
+      <div className='Badge__section-info'>
+        <h3>{jobTitle}</h3>
+        <div>@{twitter}</div>
+      </div>
+
+      <div className='Badge__footer'>#platziconf</div>
+    </div>
+  );
 }
 
 export default Badge;
